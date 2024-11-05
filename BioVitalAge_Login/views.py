51eb5415e763522aa5a218a8ccec802ab7198d1e
vito di venosa <110898637+Vito290500@users.noreply.github.com>
2024-11-05 12:31:26 +0100

    aggiunti calcoli e stampa

--- BioVitalAge_Login/views.py
+++ BioVitalAge_Login/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from .utils import calculate_biological_age 
 
 
 class MainPageView(View):
@@ -35,10 +36,48 @@ class CalcolatoreView(View):
 
     def post(self, request):
 
-        print(request.POST)
-        #Qui vanno i calcoli per calcolare l'età biologica e salvarli all'interno del database 
+        data = {key: value for key, value in request.POST.items() if key != 'csrfmiddlewaretoken'}
 
-        return render(request, "includes/calcolatore.html")
+        chronological_age = int(data.get('chronological_age'))
+        obri_index = float(data.get('obri_index'))
+        d_roms = float(data.get('d_roms'))
+        aa_epa = float(data.get('aa_epa'))
+        aa_dha = float(data.get('aa_dha'))
+        homa_test = float(data.get('homa_test'))
+        cardiovascular_risk = float(data.get('cardiovascular_risk'))
+        osi = float(data.get('osi'))
+        pat = float(data.get('pat'))
+        
+        glucose = float(data.get('glucose'))
+        creatinine = float(data.get('creatinine'))
+        ferritin = float(data.get('ferritin'))
+        albumin = float(data.get('albumin'))
+        protein = float(data.get('protein'))
+        bilirubin = float(data.get('bilirubin'))
+        uric_acid = float(data.get('uric_acid'))
+
+        exams = [glucose, creatinine, ferritin, albumin, protein, bilirubin, uric_acid]
+
+
+        # Calcola l'età biologica
+        biological_age = calculate_biological_age(
+                chronological_age,
+                obri_index, d_roms,
+                aa_epa, aa_dha,
+                homa_test,
+                cardiovascular_risk,
+                osi,
+                pat,
+                exams
+        )
+
+        context = {
+            "show_modal": True,
+            "biological_age": biological_age,
+            "data" : data
+        }
+
+        return render(request, "includes/calcolatore.html", context)
 
 
 class RisultatiView(View):
